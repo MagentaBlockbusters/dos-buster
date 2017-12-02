d97cf3a90f7d6ebcc2a650454c7f0eafd8bef3ce
roflrox <rolf.kimmelmann@gmx.de>
2017-12-02 10:47:15 +0100

    some work on api

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var mylogger = require('../logging');
+var IOTA = require('iota.lib.js');
 
+var iota = new IOTA({
+    'provider'  : 'http://localhost:14265',
+    'sandbox'   :  true,
+    'token'     : 'EXAMPLE-TOKEN-HERE'
+});
 
 
 router.route('/sendHttp').put(function(req,res){
     mylogger.debug('sendHTTP called');
     var body = req.body   
-    return res.json(body);  
+   
+    var message=body;
+    
+        console.log(message);
+        var messageStringified = JSON.stringify(message);
+        console.log(messageStringified);
+        // Convert the string to trytes
+        messageTrytes = iota.utils.toTrytes(messageStringified);
+
+    return res.json(messageTrytes);  
 
 
     //iota get sender adress count # Access
@@ -50,7 +65,22 @@ router.route('/sendCertificate').put(function(req,res){
 router.route('/getAddress').put(function(req,res){
     //iota get sender adress count # Access
     var body = req.body   
-    return res.json(body);  
+    var seed = body.seed
+
+    
+        // Deterministically generates a new address for the specified seed with a checksum
+        iota.api.getNewAddress( seed, { 'checksum': true }, function( e, address ) {
+            if (!e) {
+                mylogger.debug("Generated new address: ", address)
+                address = address
+            } else {
+                mylogger.debug("Something went wrong: ", e);
+            }
+        })
+      
+      
+
+    return res.json(address);  
 })
 
 
